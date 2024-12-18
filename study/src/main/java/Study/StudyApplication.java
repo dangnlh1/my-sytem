1482a95839b5616827729b6e41c326a6f8a7626c
haidangfaber <nguyenlehaidang@fabercompany.co.jp>
2024-12-18 21:32:44 +0700

    study config two schema

--- study/src/main/java/Study/StudyApplication.java
+++ study/src/main/java/Study/StudyApplication.java
@@ -4,7 +4,9 @@ import Study.config_mysql.SecondarySchemaRepository;
 import Study.config_mysql.mysql_common.PrimaryCommonRepository;
 import Study.config_mysql.mysql_common.SecondaryCommonRepository;
 import Study.config_mysql.primary.PrimaryEntity;
+import Study.config_mysql.primary.PrimaryRepository;
 import Study.config_mysql.secondary.SecondaryEntity;
+import Study.config_mysql.secondary.SecondaryRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +14,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudyApplication {
 
-    public StudyApplication(PrimaryCommonRepository primaryCommonRepository, SecondaryCommonRepository secondaryCommonRepository) {
+    public StudyApplication(PrimaryCommonRepository primaryCommonRepository, SecondaryCommonRepository secondaryCommonRepository, PrimaryRepository primaryRepository, SecondaryRepository secondaryRepository) {
         this.primaryCommonRepository = primaryCommonRepository;
         this.secondaryCommonRepository = secondaryCommonRepository;
+        this.primaryRepository = primaryRepository;
+        this.secondaryRepository = secondaryRepository;
     }
 
     public static void main(String[] args) {
@@ -23,12 +27,18 @@ public class StudyApplication {
 
 	private final PrimaryCommonRepository primaryCommonRepository;
 	private final SecondaryCommonRepository secondaryCommonRepository;
+	private final PrimaryRepository primaryRepository;
+	private final SecondaryRepository secondaryRepository;
 	@PostConstruct
 	void testCommonRepository (){
 		PrimaryEntity entity1 = new PrimaryEntity();
 		primaryCommonRepository.save(entity1);
+		PrimaryEntity entity2 = new PrimaryEntity();
+		primaryRepository.save(entity2);
 
-		SecondaryEntity entity2 = new SecondaryEntity();
-		secondaryCommonRepository.save(entity2);
+		SecondaryEntity entity3 = new SecondaryEntity();
+		secondaryCommonRepository.save(entity3);
+		SecondaryEntity entity4 = new SecondaryEntity();
+		secondaryRepository.save(entity4);
 	}
 }
